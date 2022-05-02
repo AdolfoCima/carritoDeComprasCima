8fad12b50770321ed296478171b78058c5b58ff0
AdolfoCima <adolfo.cima@gmail.com>
2022-05-02 18:04:37 -0300

    optimizo el codigo

--- JS/main.js
+++ JS/main.js
@@ -2,65 +2,65 @@
 const products = [
   {
     id: 1,
-    img: "img/gd5480_app_photo_front-center_white.jpg",
-    title: "CAMPERA ADIDAS ORIGINALS",
-    price: "$20000",
+    img: 'img/gd5480_app_photo_front-center_white.jpg',
+    title: 'CAMPERA ADIDAS ORIGINALS',
+    price: '$20000',
   },
   {
     id: 2,
-    img: "img/converseChuckTylor.jpg",
-    title: "CONVERSE CHUCKTYLOR",
-    price: "$12500",
+    img: 'img/converseChuckTylor.jpg',
+    title: 'CONVERSE CHUCKTYLOR',
+    price: '$12500',
   },
   {
     id: 3,
-    img: "img/adidasForumMid.jpg",
-    title: "ADIDAS FORUM MID",
-    price: "$28997",
+    img: 'img/adidasForumMid.jpg',
+    title: 'ADIDAS FORUM MID',
+    price: '$28997',
   },
   {
     id: 4,
-    img: "img/gorraAdidasOriginals.jpg",
-    title: "GORRA ORIGINALS",
-    price: "$6000",
+    img: 'img/gorraAdidasOriginals.jpg',
+    title: 'GORRA ORIGINALS',
+    price: '$6000',
   },
   {
     id: 5,
-    img: "img/product1.jpg",
-    title: "REMERA AEROREADY",
-    price: "$5997",
+    img: 'img/product1.jpg',
+    title: 'REMERA AEROREADY',
+    price: '$5997',
   },
   {
     id: 6,
-    img: "img/product4.jpg",
-    title: "BOTELLA DEPORTIVA",
-    price: "$3799",
+    img: 'img/product4.jpg',
+    title: 'BOTELLA DEPORTIVA',
+    price: '$3799',
   },
   {
     id: 7,
-    img: "img/headphonesAdidas.jpg",
-    title: "HEADSET ADIDAS",
-    price: "$32799",
+    img: 'img/headphonesAdidas.jpg',
+    title: 'HEADSET ADIDAS',
+    price: '$32799',
   },
   {
     id: 8,
-    img: "img/Maleta_Deportiva_Tiro_Primegreen_Mediana_Azul_GH7267_01_standard.jpg",
-    title: "MALETA DEPORTIVA",
-    price: "$10799",
+    img: 'img/Maleta_Deportiva_Tiro_Primegreen_Mediana_Azul_GH7267_01_standard.jpg',
+    title: 'MALETA DEPORTIVA',
+    price: '$10799',
   },
 ];
 
 let cartProducts = [];
-if (window.localStorage.getItem("chCart")) {
-  cartProducts = JSON.parse(window.localStorage.getItem("chCart"));
+if (window.localStorage.getItem('chCart')) {
+  cartProducts = JSON.parse(window.localStorage.getItem('chCart'));
   renderCart();
   updateTotal();
 }
 
-const shopContent = document.querySelector(".shop-content");
+const shopContent = document.querySelector('.shop-content');
 
 //Cargar productos
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   showProducts();
 });
 
@@ -68,24 +68,24 @@ document.addEventListener("DOMContentLoaded", () => {
 function showProducts() {
   products.forEach((product) => {
     //Scripting
-    const productBox = document.createElement("div");
-    productBox.classList.add("product-box");
+    const productBox = document.createElement('div');
+    productBox.classList.add('product-box');
 
-    const productImg = document.createElement("img");
+    const productImg = document.createElement('img');
     productImg.src = product.img;
-    productImg.classList.add("product-img");
+    productImg.classList.add('product-img');
 
-    const productTitle = document.createElement("h2");
+    const productTitle = document.createElement('h2');
     productTitle.textContent = product.title;
-    productTitle.classList.add("product-title");
+    productTitle.classList.add('product-title');
 
-    const productPrice = document.createElement("span");
+    const productPrice = document.createElement('span');
     productPrice.textContent = product.price;
-    productPrice.classList.add("price");
+    productPrice.classList.add('price');
 
-    const productButton = document.createElement("i");
-    productButton.className = "bx bx-shopping-bag add-cart";
-    productButton.addEventListener("click", () => addCartCLicked(product));
+    const productButton = document.createElement('i');
+    productButton.className = 'bx bx-shopping-bag add-cart';
+    productButton.addEventListener('click', () => addCartCLicked(product));
 
     productBox.appendChild(productImg);
     productBox.appendChild(productTitle);
@@ -97,38 +97,42 @@ function showProducts() {
 }
 
 //Carrito
-let cartIcon = document.querySelector("#cart-icon");
-let cart = document.querySelector(".cart");
-let closeCart = document.querySelector("#close-cart");
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
 
 //Abrir Carrito
 cartIcon.onclick = () => {
-  cart.classList.add("active");
+  cart.classList.add('active');
 };
 
 //Cerrar Carrito
 closeCart.onclick = () => {
-  cart.classList.remove("active");
+  cart.classList.remove('active');
 };
 
 //Carrito funcionando
-if (document.readyState == "loading") {
-  document.addEventListener("DOMContentLoaded", ready);
-} else {
-  ready();
-}
+
+// if (document.readyState == 'loading') {
+//   document.addEventListener('DOMContentLoaded', ready);
+// } else {
+//   ready();
+// }
+document.readyState == 'loading'
+  ? document.addEventListener('DOMContentLoaded', ready)
+  : ready();
 
 //Funcion ready
 function ready() {
   //Boton Comprar ahora
   document
-    .getElementsByClassName("btn-buy")[0]
-    .addEventListener("click", buyButtonClicked);
+    .getElementsByClassName('btn-buy')[0]
+    .addEventListener('click', buyButtonClicked);
 }
 
 //Funcion buyButtonClicked
 function buyButtonClicked() {
-  alert("Gracias por su compra!!!");
+  alert('Gracias por su compra!!!');
   cartProducts = [];
   renderCart();
   updateLocalStorage();
@@ -158,7 +162,7 @@ function quantityChanged(event, product) {
 function addCartCLicked(product) {
   product.quantity = 1;
   if (cartProducts.find((productInCart) => productInCart.id === product.id)) {
-    alert("Ya agregaste este item al carrito");
+    alert('Ya agregaste este item al carrito');
   } else {
     cartProducts.push(product);
     renderCart();
@@ -170,17 +174,17 @@ function addCartCLicked(product) {
 ///////////////// Agregar productos al carrito
 
 function renderCart() {
-  const cartItems = document.getElementsByClassName("cart-content")[0];
-  cartItems.innerHTML = "";
+  const cartItems = document.getElementsByClassName('cart-content')[0];
+  cartItems.innerHTML = '';
   cartProducts.forEach((product, index) => {
     renderProduct(product, index);
   });
 }
 
 function renderProduct(product, i) {
-  let cartShopBox = document.createElement("div");
-  cartShopBox.classList.add("cart-box");
-  const cartItems = document.getElementsByClassName("cart-content")[0];
+  let cartShopBox = document.createElement('div');
+  cartShopBox.classList.add('cart-box');
+  const cartItems = document.getElementsByClassName('cart-content')[0];
   let cartBoxContent = `<img src="${product.img}" alt="" class="cart-img" />
                         <div class="detail-box">
                           <div class="cart-product-title">${product.title}</div>
@@ -191,27 +195,26 @@ function renderProduct(product, i) {
   cartShopBox.innerHTML = cartBoxContent;
   cartItems.append(cartShopBox);
   cartShopBox
-    .getElementsByClassName("cart-remove")[0]
-    .addEventListener("click", () => removeCartItem(i));
+    .getElementsByClassName('cart-remove')[0]
+    .addEventListener('click', () => removeCartItem(i));
   cartShopBox
-    .getElementsByClassName("cart-quantity")[0]
-    .addEventListener("change", (event) => quantityChanged(event, product));
+    .getElementsByClassName('cart-quantity')[0]
+    .addEventListener('change', (event) => quantityChanged(event, product));
 }
 
 ////////////////////////Actualizar total
 function updateTotal() {
-
   let total = cartProducts.reduce((acumulador, product) => {
-    const price = +product.price.replace("$", "");
+    const price = +product.price.replace('$', '');
     return acumulador + price * product.quantity;
   }, 0);
 
   //Redondear precios
   total = Math.round(total * 100) / 100;
   //console.log(total);
-  document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+  document.getElementsByClassName('total-price')[0].innerText = '$' + total;
 }
 
 function updateLocalStorage() {
-  window.localStorage.setItem("chCart", JSON.stringify(cartProducts));
+  window.localStorage.setItem('chCart', JSON.stringify(cartProducts));
 }
